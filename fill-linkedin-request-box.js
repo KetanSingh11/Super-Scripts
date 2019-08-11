7743cbeef9cb77e36e02e279970b832d73c039a1
Ketan Singh <ketansingh_vsec@yahoo.com>
2019-08-11 20:11:27 +0530

    updating minify, adding name to text filler

--- fill-linkedin-request-box.js
+++ fill-linkedin-request-box.js
@@ -1,6 +1,18 @@
+function get_profile_first_name(){
+    var member_name_class_name = "pv-top-card-section__name inline t-24 t-black t-normal";
+    var name_list = document.getElementsByClassName(member_name_class_name);
+    
+    var profile_name = name_list[0].innerText.trim();
+    
+    // return only FIRST name
+    var first_name = profile_name.split(" ")[0].trim();
+    console.log("First Name: " + first_name);
+    return first_name;
+}
+
 function fill_in_message(first_name){
 var message_template = "Hello <>! I'd like to add you to my growing professional network on LinkedIn as a valued connection. Please kindly accept my connection request.";
-var message_txt = message_template
+var message_txt = message_template.replace("<>", first_name);
 
 var text_box_element_name = "message";
 var txt_box_list = document.getElementsByName(text_box_element_name);
@@ -12,4 +24,4 @@ console.log("text_box filled...");
 
 
 // fill just message in text box
-fill_in_message();
+fill_in_message(get_profile_first_name());
--- minify.sh
+++ minify.sh
@@ -1,4 +1,41 @@
 #!/bin/bash
-echo "minifying autosend-linkedin-requests.js ..."
-uglifyjs autosend-linkedin-requests.js -o autosend-linkedin-requests.min.js
-echo "...Done"
+minify_folder="minified"    # static folder
+
+function get_new_filename {
+    # echo "inside"
+    datetime="$(date +%Y-%m-%d_%H-%M-%S)"
+
+    # echo "--  $1 "
+    fullname=$(basename -- $1)
+    # echo $fullname
+
+    extension="${fullname##*.}"
+    # echo $extension
+    
+    filename="${fullname%.*}"
+    # echo $filename
+    
+    local new_filename=$filename"_"$datetime".js"
+    
+    # returning value
+    echo $new_filename
+}
+
+
+if [ $# -eq  "0" ]; then
+     echo "Syntax Error: No file supplied"
+     echo -e "usage: \n$0 <somefile.js>\n"
+else
+    echo -e "minifying '$1' ...\n"
+    new_file=$(get_new_filename $1)
+    # echo $new_file
+
+    output_file=$minify_folder"/"$new_file
+
+    echo -e "output: $output_file\n"
+
+    # Example:: uglifyjs $1 -o autosend-linkedin-requests.min.js
+    uglifyjs "$1" --output "$output_file"
+    
+    echo -e "\n...Done!"
+fi
